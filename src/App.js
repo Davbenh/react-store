a129ee67dc1ebf60f7483cca099393de797874ea
dudu benh <dav_bh@outlook.com>
2022-11-28 21:48:36 +0200

    added total price and total qnty to cart

--- src/App.js
+++ src/App.js
@@ -6,43 +6,43 @@ import Header from "./components/Header";
 import Cart from "./components/Cart";
 import axios from "axios";
 
-
-
-
 function App() {
   const [sidebarOpen, setSideBarOpen] = useState(false);
   const [cart, setCart] = useState([]);
   const [dataItems, setDataItems] = useState([]);
+  const [cartQty, setCartQty] = useState(0);
+  const [cartSum, setCartSum] = useState(0);
 
   const handleViewSidebar = () => {
     setSideBarOpen(!sidebarOpen);
   };
 
-
-
   const addToCart = (id) => {
-    const itemAdd = dataItems.filter((item) =>  item.id == id)
-    const isInCart = cart.findIndex((item) =>  item.id == id)
+    const itemAdd = dataItems.filter((item) => item.id == id);
+    const isInCart = cart.findIndex((item) => item.id == id);
     if (isInCart < 0) {
       itemAdd[0].qty += 1;
-      setCart(current => [...current, ...itemAdd]);
-      setSideBarOpen("sidebar open")
+      setCart((current) => [...current, ...itemAdd]);
+      setSideBarOpen("sidebar open");
     } else {
       cart[isInCart].qty += 1;
-      setCart(current => [...current]);
-      setSideBarOpen("sidebar open")
+      setCart((current) => [...current]);
+      setSideBarOpen("sidebar open");
     }
-    } 
+  };
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setDataItems(json.map(v => ({...v, qty: v.value = Number(0)}))))
-    
+      .then((json) =>
+        setDataItems(json.map((v) => ({ ...v, qty: (v.value = Number(0)) })))
+      );
   }, []);
 
   return (
     <div className="App">
-      <ContextData.Provider value={{ dataItems, cart, setCart,addToCart }}>
+      <ContextData.Provider
+        value={{ dataItems, cart, setCart, addToCart, cartQty, cartSum }}
+      >
         <Header onClick={handleViewSidebar} />
         <Cart isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
         <Main />
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../App.css";
 import { ContextData } from "../App";
 
 const Cart = (props) => {
-  const { dataItems, cart, setCart, addToCart } = useContext(ContextData);
+  const { dataItems, cart, setCart, addToCart, cartSum, cartQty } =
+    useContext(ContextData);
 
   const sidebarClass = props.isOpen ? "sidebar open" : "sidebar";
   return (
     <div className={sidebarClass}>
-       <div className="cart-item">
-      {cart.map((item, index) => {
-        return (
-          
+      <div className="cart-item">
+        {cart.map((item, index) => {
+          return (
             <div className="cart-item-header" key={index}>
               <img
                 src={item.image}
@@ -22,10 +22,18 @@ const Cart = (props) => {
               <div className="cart-item-price">price: ${item.price}</div>
               <div className="cart-item-qty">total: {item.qty}</div>
             </div>
-        );
-      })}
+          );
+        })}
       </div>
-      <div className="cart-item-footer">dasdasdd</div>
+      <div className="cart-item-footer">
+        <div className="cartQty">
+          total price : $
+          {cart.reduce((total, item) => total + item.price * item.qty, 0)}
+        </div>
+        <div className="cartSum">
+          total pcs : {cart.reduce((total, item) => total + item.qty, 0)}
+        </div>
+      </div>
     </div>
   );
 };
